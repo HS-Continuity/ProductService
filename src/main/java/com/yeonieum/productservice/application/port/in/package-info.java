019baaa5732c1584aab30d3874c91e67bf89978a
kimujae <dnwo0103@naver.com>
2024-07-03 14:54:00 +0900

    init : 프로젝트 패키지 구조 및 config 설정

---
+++ src/main/java/com/yeonieum/productservice/application/port/in/package-info.java
@@ -0,0 +1,4 @@
+package com.yeonieum.productservice.application.port.in;
+/**
+ * use case 기반의 application 입력 포트 클래스가 정의된 패키지
+ * **/
---
+++ src/main/java/com/yeonieum/productservice/domain/package-info.java
@@ -0,0 +1,4 @@
+package com.yeonieum.productservice.domain;
+/**
+ * 순수 도메인 객체 클래스로 구성된 패키지
+ * **/
