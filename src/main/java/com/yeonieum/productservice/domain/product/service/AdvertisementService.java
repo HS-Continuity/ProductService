b8922582cd88fda0bdb14d21ea57f4586cdbccb1
kimujae <dnwo0103@naver.com>
2024-07-05 14:34:47 +0900

    KAN-147 <상품광고관리서비스 구현>

--- src/main/java/com/yeonieum/productservice/domain/product/service/AdvertisementService.java
+++ src/main/java/com/yeonieum/productservice/domain/product/service/AdvertisementService.java
@@ -20,8 +20,12 @@ public class AdvertisementService {
     private final CustomerRepository customerRepository;
     private final ProductRepository productRepository;
     private final ProductAdvertisementServiceRepository productAdvertisementServiceRepository;
-    // 상당노출신청상품리스트 조회
-    // 상단노출신청하기
+
+    /**
+     * 상당노출신청상품리스트 조회
+     * @param customerId
+     * @return
+     */
     public List<RetrieveAdvertisementProductResponseDto> retrieveAppliedProduct(Long customerId) {
         Customer customer =
                 customerRepository.findById(customerId).orElseThrow(
@@ -33,6 +37,11 @@ public class AdvertisementService {
         return advertisementProduct;
     }
 
+    /**
+     * 상품광고서비스 신청
+     * @param registerAdvertisementRequestDto
+     * @return
+     */
     public boolean registerAdvertisement(RegisterAdvertisementRequestDto registerAdvertisementRequestDto) {
         Product product = productRepository.findById(registerAdvertisementRequestDto.getProductId()).orElseThrow(
                 () -> new IllegalArgumentException("해당하는 상품이 존재하지 않습니다.")
