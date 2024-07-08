43c3a24cfccb26d8401802cec3bbc9c7e1abe842
kimujae <dnwo0103@naver.com>
2024-07-08 16:19:55 +0900

    KAN-150 feat: 상품재고서비스 구현-주문서비스와 메시지통신을 위한 카프카 consumer 구현(출고대기 -> 주문취소이벤트, 출고지시이벤트)

---
+++ src/main/java/com/yeonieum/productservice/messaging/service/KafkaConsumerService.java
@@ -0,0 +1,28 @@
+package com.yeonieum.productservice.messaging.service;
+
+import com.yeonieum.productservice.cache.redis.StockRedisSetOperation;
+import com.yeonieum.productservice.domain.productinventory.dto.ShippedStockDto;
+import com.yeonieum.productservice.domain.productinventory.dto.StockUsageDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.kafka.annotation.KafkaListener;
+
+//@Service
+@RequiredArgsConstructor
+public class KafkaConsumerService {
+    private final StockRedisSetOperation stockRedisSetOperation;
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    @KafkaListener(groupId = "shippedstock_group", topics = "shippedstock-topic", containerFactory = "shippedStockKafkaListenerContainerFactory")
+    public void listen(ShippedStockDto shippedStockDto) {
+        //System.out.println(shippedStockDto);
+        stockRedisSetOperation.addShippedStock(redisTemplate, shippedStockDto);
+    }
+
+
+    @KafkaListener(groupId = "stockusage_group", topics = "stockusage-topic", containerFactory = "stockUsageKafkaListenerContainerFactory")
+    public void listen(StockUsageDto stockUsageDto) {
+        //System.out.println(shippedStockDto);
+        stockRedisSetOperation.removeStockUsage(redisTemplate, stockUsageDto);
+    }
+}
