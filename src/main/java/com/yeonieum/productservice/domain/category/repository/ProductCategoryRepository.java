65df921588036606899bb65fda6520605a1f2acc
Koo-Tae-Ho <gth0908@naver.com>
2024-07-08 12:18:45 +0900

    KAN-199 refactor: 패치조인사용으로 성능최적화

--- src/main/java/com/yeonieum/productservice/domain/category/repository/ProductCategoryRepository.java
+++ src/main/java/com/yeonieum/productservice/domain/category/repository/ProductCategoryRepository.java
@@ -2,6 +2,15 @@ package com.yeonieum.productservice.domain.category.repository;
 
 import com.yeonieum.productservice.domain.category.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Long> {
+
+    @Query("SELECT c FROM ProductCategory c LEFT JOIN FETCH c.productDetailCategoryList WHERE c.productCategoryId = :productCategoryId")
+    Optional<ProductCategory> findByIdWithDetailCategories(@Param("productCategoryId") Long productCategoryId);
 }
+
