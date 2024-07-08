4dace904dee620efc293afa211e4d3422680ba72
kimujae <dnwo0103@naver.com>
2024-07-08 16:04:22 +0900

    KAN-150 feat: 상품재고서비스 구현-주문서비스와 메시지 통신하기 위한 카프카 설정

--- src/main/java/com/yeonieum/productservice/global/config/KafkaConsumerConfig.java
+++ src/main/java/com/yeonieum/productservice/global/config/KafkaConsumerConfig.java
@@ -1,6 +1,9 @@
 package com.yeonieum.productservice.global.config;
 
+import com.yeonieum.productservice.domain.productinventory.dto.ShippedStockDto;
+import com.yeonieum.productservice.domain.productinventory.dto.StockUsageDto;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -18,21 +21,40 @@ import java.util.Map;
 public class KafkaConsumerConfig {
 
     @Bean
-    public ConsumerFactory<String, Object> consumerFactory() {
+    public ConsumerFactory<String, ShippedStockDto> shippedStockConsumerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "tmpTopic");
-        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
-        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
-        configProps.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class.getName());
-        configProps.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-        return new DefaultKafkaConsumerFactory<>(configProps);
+
+        return new DefaultKafkaConsumerFactory<>(configProps, new StringDeserializer(), new JsonDeserializer<>(ShippedStockDto.class));
+
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
+    public ConcurrentKafkaListenerContainerFactory<String, ShippedStockDto> shippedStockKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, ShippedStockDto> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(shippedStockConsumerFactory());
+
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, StockUsageDto> stockUsageConsumerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "tmpTopic");
+
+        return new DefaultKafkaConsumerFactory<>(configProps, new StringDeserializer(), new JsonDeserializer<>(StockUsageDto.class));
+
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, StockUsageDto> stockUsageKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, StockUsageDto> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(stockUsageConsumerFactory());
+
         return factory;
     }
 }
