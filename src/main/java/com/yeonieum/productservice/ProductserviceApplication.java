6c874b67b2433b34d767dd417eef70f239df8991
kimujae <dnwo0103@naver.com>
2024-07-08 16:40:16 +0900

    KAN-150 refactor: 상품재고서비스 구현-레디슨클라이언트 빈등록하지 않도록 제외

--- src/main/java/com/yeonieum/productservice/ProductserviceApplication.java
+++ src/main/java/com/yeonieum/productservice/ProductserviceApplication.java
@@ -1,9 +1,10 @@
 package com.yeonieum.productservice;
 
+import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = RedissonAutoConfiguration.class)
 public class ProductserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductserviceApplication.class, args);
