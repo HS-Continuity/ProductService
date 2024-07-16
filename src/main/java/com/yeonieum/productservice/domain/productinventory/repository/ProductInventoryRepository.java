b56b8b2ab1efe4dc5bd3599b6823411185e6bbfe
kimujae <dnwo0103@naver.com>
2024-07-16 18:33:41 +0900

    KAN-280 refactor: 재고현황관리 레디스 리팩토링-bulk 조회 및 레디스 파이프라인 조회 구현

--- src/main/java/com/yeonieum/productservice/domain/productinventory/repository/ProductInventoryRepository.java
+++ src/main/java/com/yeonieum/productservice/domain/productinventory/repository/ProductInventoryRepository.java
@@ -1,7 +1,9 @@
 package com.yeonieum.productservice.domain.productinventory.repository;
 
+import com.yeonieum.productservice.domain.product.entity.Product;
 import com.yeonieum.productservice.domain.productinventory.dto.RetrieveProductInventoryResponse;
 import com.yeonieum.productservice.domain.productinventory.entity.ProductInventory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,7 +11,6 @@ import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 public interface ProductInventoryRepository extends JpaRepository<ProductInventory, Long> {
     @Query(value = "SELECT" +
@@ -21,7 +22,21 @@ public interface ProductInventoryRepository extends JpaRepository<ProductInvento
             nativeQuery = true)
     List<RetrieveProductInventoryResponse> findAllbyProductId(@Param("productId") Long productId , Pageable pageable);
 
+    //@Cacheable(value = "availableQuantity", key = "#productId + '-' + #expirationDate", cacheManager = "servercacheManager")
     @Query(value = "SELECT COALESCE(SUM(pi.quantity), 0) FROM product_inventory pi WHERE pi.product_id = :productId AND pi.expiration_date > :expirationDate", nativeQuery = true)
     int findAvailableInventoryQuantityByProductIdAndExpirationDate(@Param("productId") Long productId, @Param("expirationDate") LocalDate expirationDate);
+
+
+    @Query(value = "SELECT pi.product_id, COALESCE(SUM(pi.quantity), 0) " +
+            "FROM product_inventory pi " +
+            "WHERE (pi.product_id, pi.expiration_date) IN (:productExpirations) " +
+            "GROUP BY pi.product_id",
+            nativeQuery = true)
+    List<Object[]> findInventorySumByProductsAndExpirations(@Param("productExpirations") List<Object[]> productExpirations);
+    @Query(value = "SELECT COALESCE(SUM(pi.quantity), 0) " +
+            "FROM product_inventory pi " +
+            "WHERE pi.product_id IN :productIds " +
+            "AND pi.expiration_date IN :expirationDates", nativeQuery = true)
+    List<Integer> bulkFindAvailableInventoryQuantityByProductIdAndExpirationDate(@Param("productIds") List<Long> productId, @Param("expirationDates") List<LocalDate> expirationDate);
 
 }
--- src/main/java/com/yeonieum/productservice/domain/productinventory/service/ProductDetailCategoryService.java
+++ src/main/java/com/yeonieum/productservice/domain/productinventory/service/ProductDetailCategoryService.java
@@ -1,13 +1,13 @@
-package com.yeonieum.productservice.domain.productinventory.repository;
+package com.yeonieum.productservice.domain.productinventory.service;
 
 import com.yeonieum.productservice.domain.product.entity.Product;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Cache {
-    @Cacheable(value = "shelflife:", key = "#product.getProductId()")
-    public int cache(Product product) {
+public class ProductDetailCategoryService {
+    @Cacheable(value = "shelflife:", key = "#product.getProductId()", cacheManager = "localCacheManager")
+    public int getProductShelflife(Product product) {
         int lifeDay = product.getProductDetailCategory().getShelfLifeDay();
         return lifeDay;
     }
