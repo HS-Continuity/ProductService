1ee0b41531d82c5ecf48c71981534d083537da95
kimujae <dnwo0103@naver.com>
2024-07-23 15:09:46 +0900

    KAN-483 feat: MSA인프라-API권한 데이터 반환 컨트롤러 구현

--- src/test/java/com/yeonieum/productservice/ProductserviceApplicationTests.java
+++ src/test/java/com/yeonieum/productservice/ProductserviceApplicationTests.java
@@ -1,52 +1,12 @@
 package com.yeonieum.productservice;
-
-import com.yeonieum.productservice.domain.productinventory.service.StockSystemService;
 import org.junit.jupiter.api.Test;
-import org.redisson.api.RedissonClient;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.concurrent.*;
 
-//@SpringBootTest
+@SpringBootTest
 class ProductserviceApplicationTests {
-    @Autowired
-    private StockSystemService stockSystemService;
-    @Autowired
-    private  RedissonClient redissonClient;
+   @Test
+   void contextLoads(){}
 }
-
-
-
-
-//   @Test
-//   void contextLoads() throws InterruptedException {
-//      int numUsers = 100;
-//      ExecutorService executorService = Executors.newFixedThreadPool(numUsers);
-//      for (int i = 1; i <= numUsers; i++) {
-//         System.out.println(i);
-//         int finalI = i;
-//         executorService.submit(() -> {
-//            // 각 사용자가 실행할 작업 정의 (예: 숫자 증가)
-//            System.out.println(111);
-//            StockUsageDto stockUsageDto = StockUsageDto.builder()
-//                  .productId(1L)
-//                  .orderId((long) finalI)
-//                  .quantity(1)
-//                  .build();
-//            stockSystemService.processProductInventory(AvailableProductInventoryRequest.IncreaseStockUsageDto.builder()
-//                  .productId(1L)
-//                  .orderId((long) finalI)
-//                  .quantity(10)
-//                  .build());
-//            return 1;
-//         });
-//      }
-//
-//      // 모든 사용자가 실행을 완료할 때까지 기다림
-//      executorService.shutdown();
-//      while (!executorService.isTerminated()) {
-//         Thread.sleep(100); // 잠시 대기
-//      }
-//   }
 
 
