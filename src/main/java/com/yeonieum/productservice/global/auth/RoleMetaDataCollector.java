709b4df2f28ed7583ba70d7d2ccff4fbf02c8a4d
kimujae <dnwo0103@naver.com>
2024-07-23 11:34:28 +0900

    KAN-481 feat: MSA인프라-애너테이션 기반 롤식별을 위한 애너테이션과 롤메타데이터수집기 구현

---
+++ src/main/java/com/yeonieum/productservice/global/auth/RoleMetaDataCollector.java
@@ -0,0 +1,47 @@
+package com.yeonieum.productservice.global.auth;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class RoleMetaDataCollector {
+
+    private final Map<String, List<String>> roleMetadata = new HashMap<>();
+    private final ApplicationContext applicationContext;
+
+    @Autowired
+    public RoleMetaDataCollector(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @PostConstruct
+    public void collectRoleMetadata() {
+        // 모든 @RestController 또는 @Controller 빈 가져오기
+        Map<String, Object> beans = applicationContext.getBeansWithAnnotation(RestController.class);
+
+        for (Object bean : beans.values()) {
+            Class<?> beanClass = bean.getClass();
+            Method[] methods = beanClass.getDeclaredMethods();
+            for (Method method : methods) {
+                Role secured = method.getAnnotation(Role.class);
+                if (secured != null) {
+                    String methodName = beanClass.getName() + "." + method.getName();
+                    roleMetadata.put(secured.url(), Arrays.asList(secured.value()));
+                }
+            }
+        }
+    }
+
+    public Map<String, List<String>> getRoleMetadata() {
+        return roleMetadata;
+    }
+}
