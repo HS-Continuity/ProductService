70226dd310dceaa77e54d08b58e8a87a24016505
kimujae <dnwo0103@naver.com>
2024-08-02 20:11:08 +0900

    KAN-701 feat: 최종배포를 위한 CORS 프로퍼티 주입설정과 배포 스크립트 추가

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,22 @@
+# Use Eclipse Temurin JDK for the base image
 FROM eclipse-temurin:17-jdk-alpine
+
+# Set environment variables
+ENV SPRING_OUTPUT_ANSI_ENABLED=ALWAYS
+
+# Update and install necessary packages
+USER root
+RUN apk update && apk add --no-cache curl jq
+
+# Copy the JAR file into the container
 ARG JAR_FILE=/build/libs/*-SNAPSHOT.jar
-COPY ${JAR_FILE} yeonieum_productservice.jar
-ENTRYPOINT ["java","-jar","/yeonieum_productservice.jar"]
+COPY ${JAR_FILE} /yeonieum_productservice.jar
+
+# Copy the entrypoint script into the container
+COPY entrypoint.sh /entrypoint.sh
+
+# Expose the port
+EXPOSE 8070
+
+# Set the entrypoint
+ENTRYPOINT ["sh", "/entrypoint.sh"]
--- src/main/java/com/yeonieum/productservice/global/config/CorsConfig.java
+++ src/main/java/com/yeonieum/productservice/global/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.yeonieum.productservice.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,7 +10,8 @@ import java.util.Arrays;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
+    @Value("${cors.allowed.origin}")
+    private String CORS_ALLOWED_ORIGIN;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
@@ -17,11 +19,11 @@ public class CorsConfig implements WebMvcConfigurer {
         corsConfiguration.setAllowedHeaders(Arrays.asList("Content-Type", "application/json", "Authorization", "Bearer"));
         corsConfiguration.addExposedHeader("Bearer");
         corsConfiguration.addExposedHeader("provider");
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174", "http://localhost:3000"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174", "http://localhost:3000",CORS_ALLOWED_ORIGIN));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS"));
 
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:3000")
+                .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:3000", CORS_ALLOWED_ORIGIN)
                 .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS")
                 .allowedHeaders("Content-Type", "application/json", "Authorization", "Bearer")
                 .exposedHeaders("Bearer", "provider")
